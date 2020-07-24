79a7219a2c739e2022df1ea5867840a07dac2ef2
John Doe <johndoe@example.com>
2020-07-24 20:29:46 +0100

    template...

---
+++ smexport.go
@@ -0,0 +1,150 @@
+package main
+
+import (
+	"fmt"
+	"os"
+    "net/http"
+    "time"
+    "encoding/json"
+    "strings"
+)
+
+
+type DomainPagedRecords struct {
+    Num_results int  
+    Page int
+    Objects []DomainRecord
+    Total_pages int
+}
+
+type DomainRecord struct {
+    Domain string
+    Id int
+}
+
+type UrlPagedRecords struct {
+    Num_results int  
+    Page int
+    Objects []UrlRecord
+    Total_pages int
+}
+
+type UrlRecord struct {
+    Url string
+    Id int
+}
+
+func main() {
+    if os.Args[1] == "domain" {
+        firstPage := new(DomainPagedRecords) 
+        link := "http://bbsstore-service:7002/api/dns_store"
+        getJson("http://bbsstore-service:7002/api/dns_store?page=1", firstPage)
+        totalPages := firstPage.Total_pages
+        //fmt.Println(totalPages)
+        
+        for i := 1; i <= totalPages; i++ {
+            //fmt.Println(i)
+            concatenated := fmt.Sprintf("%s?page=%d", link, i)
+            //fmt.Println(concatenated)
+            
+            jsonData := new(DomainPagedRecords)
+            getJson(concatenated, jsonData)
+            
+            for currentIndex := range jsonData.Objects {
+                fmt.Println(jsonData.Objects[currentIndex].Domain)
+            }
+        }
+    }
+    
+    if os.Args[1] == "url" {
+        firstPage := new(UrlPagedRecords) 
+        link := "http://bbsstore-service:7002/api/url_store"
+        getJson("http://bbsstore-service:7002/api/url_store?page=1", firstPage)
+        totalPages := firstPage.Total_pages
+        //fmt.Println(totalPages)
+        
+        for i := 1; i <= totalPages; i++ {
+            //fmt.Println(i)
+            concatenated := fmt.Sprintf("%s?page=%d", link, i)
+            //fmt.Println(concatenated)
+            
+            jsonData := new(UrlPagedRecords)
+            getJson(concatenated, jsonData)
+            
+            for currentIndex := range jsonData.Objects {
+                fmt.Println(jsonData.Objects[currentIndex].Url)
+            }
+        }
+    }
+    
+    if os.Args[1] == "param" {
+        firstPage := new(UrlPagedRecords) 
+        link := "http://bbsstore-service:7002/api/url_store"
+        // we could improve this code by querying url_store api only for urls containing '?' and '='
+        getJson("http://bbsstore-service:7002/api/url_store?page=1", firstPage)
+        totalPages := firstPage.Total_pages
+        //totalPages = 300
+        
+        //fmt.Println(totalPages)
+        
+        //var foundParams
+        
+        var foundParams []string
+        
+        for i := 1; i <= totalPages; i++ {
+            //fmt.Println(i)
+            concatenated := fmt.Sprintf("%s?page=%d", link, i)
+            //fmt.Println(concatenated)
+            
+            jsonData := new(UrlPagedRecords)
+            getJson(concatenated, jsonData)
+            
+            
+            for currentIndex := range jsonData.Objects {
+                currentObject  := jsonData.Objects[currentIndex]
+                if strings.Contains(currentObject.Url, "?") {
+                    //fmt.Println(currentObject.Url)
+                    
+                    paramStr := strings.Split(currentObject.Url, "?")[1]
+                    params := strings.Split(paramStr, "&")
+                    for _, param := range params {
+                        // ignore any params without an =, these might be used for cache busting
+                        if strings.Contains(param, "="){
+                            paramKeyandValue := strings.Split(param, "=")
+                            paramKey := paramKeyandValue[0]
+                            //fmt.Println(paramKey)
+                            if contains(foundParams, paramKey) == false {
+                                fmt.Println(paramKey)
+                                foundParams = append(foundParams, paramKey)
+                            }
+                        }
+                    }
+                    
+                    
+                }
+            }
+        }
+    }
+}
+
+func contains(arr []string, str string) bool {
+   for _, a := range arr {
+      if a == str {
+         return true
+      }
+   }
+   return false
+}
+
+
+var myClient = &http.Client{Timeout: 10 * time.Second}
+
+func getJson(url string, target interface{}) error {
+    r, err := myClient.Get(url)
+    if err != nil {
+        return err
+    }
+    defer r.Body.Close()
+
+    return json.NewDecoder(r.Body).Decode(target)
+}
